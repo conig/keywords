18164db203f5dfb90af1c71f9632fb0295efc99b
conig <james.conigrave@gmail.com>
2020-07-13 11:10:43 +1000

    include  regex exclusion

--- R/get_words.R
+++ R/get_words.R
@@ -4,23 +4,24 @@
 #' @param folder the path to a folder
 #' @param subfolders a bool. if true, subfolders included
 #' @param unnest_tokens a bool. If true, a dataset split into one word per row is returned.
-#' @param exclude words to exclude
+#' @param multiprocess if more than 100 files requested and if this argument is TRUE, multiple R sessions will be used to speed up reading
+#' @param exclude regex for removing files
 #' @export
 
-pdf_words = function(folder, subfolders = T, unnest_tokens = FALSE, exclude = c()){
+pdf_words = function(folder, subfolders = T, multiprocess = T, unnest_tokens = FALSE, exclude = NULL){
 
-  file_names = list.files(
-    folder,
-    pattern = ".pdf",
-    recursive = subfolders,
-    full.names = F
-  )
   file_paths = list.files(
     folder,
     pattern = ".pdf",
     recursive = subfolders,
     full.names = T
   )
+
+  if(!is.null(exclude)){
+    file_paths = file_paths[!grepl(exclude, file_paths)]
+  }
+
+  file_names = gsub(".*\\/","",file_paths)
 
   if(length(file_names) == 0 & tools::file_ext(tolower(folder)) == "pdf"){
     file_names = basename(folder)
@@ -31,10 +32,18 @@ pdf_words = function(folder, subfolders = T, unnest_tokens = FALSE, exclude = c(
     stop("There aren't any .pdfs in that folder.", call. = F)
   }
 
-  corpus = lapply(seq_along(file_names), function(x) {
-    out = tibble::tibble(path = file_paths[x], content = getPdf(file_paths[x]))
+  if(length(file_names) > 50){
+    msg = glue::glue("Reading {length(file_names)} pdfs.")
+    if(multiprocess) {
+      future::plan(future::multiprocess)
+      msg <- paste(msg, "Using multiple R sessions to speed things up.")
+    }
+    message(msg)
+  }
 
-    return(out)
+  corpus = future.apply::future_lapply(seq_along(file_names), function(x) {
+    out = data.frame(path = file_paths[x], content = getPdf(file_paths[x]))
+    out
   })
 
   corpus = do.call(rbind, corpus)
