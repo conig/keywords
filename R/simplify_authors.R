004bbc8e8cd4b9e168aef885529ad0475c0040af
conig <james.conigrave@gmail.com>
2021-01-22 11:22:30 +1100

    improve simplify_authors

--- R/simplify_authors.R
+++ R/simplify_authors.R
@@ -2,11 +2,12 @@
 #'
 #' Reduce a list of authors to et al where appropriate
 #' @param x a character vector of comma separated author surnames
+#' @param split regex rules by which to split up authors.
 #' @export
 
-simplify_authors = function(x){
+simplify_authors = function(x, split = ",|&" ){
 
-  y <- strsplit(x, split = ",")
+  y <- strsplit(x, split = split)
   out <- lapply(y, function(i){
     if(length(i) == 1) return(i)
     if(length(i) == 2) return(paste(i, collapse = " & "))
