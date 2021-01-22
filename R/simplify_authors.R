d9c39d929b3a19d95eb2ab42cfdce1993a558866
conig <james.conigrave@gmail.com>
2021-01-22 11:35:28 +1100

    handle first names

--- R/simplify_authors.R
+++ R/simplify_authors.R
@@ -9,6 +9,7 @@ simplify_authors = function(x, split = ",|&" ){
 
   y <- strsplit(x, split = split)
   out <- lapply(y, function(i){
+    i <- gsub("\\,.*","",i)
     if(length(i) == 1) return(i)
     if(length(i) == 2) return(paste(i, collapse = " & "))
     return(paste(i[[1]], "et al."))
